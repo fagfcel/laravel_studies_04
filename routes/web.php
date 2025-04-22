4e29cb1864e66ef7fa5bd37f9cd790f51188d570
Francisco Ferreira <estudosfagf@gmail.com>
2025-04-22 20:49:45 -0300

    master

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','home');
+Route::get('/', function(){
+    try {
+        DB::connection()->getPdo();
+        echo "Conexão com a base de dados: ". DB::connection()->getDatabaseName();
+    } catch (Exception $e) {
+        //throw $th;
+        die("Não foi possivel conectar com a base de dados. Erro: " . $e->getMessage());
+    }
+});
