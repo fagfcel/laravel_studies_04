a833ac8aa09a5dcc567c0dec262bd3a58bd520d3
Francisco Ferreira <estudosfagf@gmail.com>
2025-04-22 21:08:03 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,21 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){
+Route::get('/mysql', function(){
     try {
         DB::connection()->getPdo();
         echo "Conexão com a base de dados: ". DB::connection()->getDatabaseName();
     } catch (Exception $e) {
         //throw $th;
+        die("Não foi possivel conectar com a base de dados. Erro: " . $e->getMessage());
+    }
+});
+
+Route::get('/sqlite', function(){
+    try {
+        DB::connection()->getPdo();
+        echo "Conexão com a base de dados: ". DB::connection()->getDatabaseName();
+    } catch (Exception $e) {
         die("Não foi possivel conectar com a base de dados. Erro: " . $e->getMessage());
     }
 });
